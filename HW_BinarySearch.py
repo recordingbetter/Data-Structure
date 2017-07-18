fd261fc4880319e30e68ee8c7d3a1d3cf88c0fb1
Joe Donghee Cho <recordingbetter@gmail.com>
2017-07-18 10:13:51 +0900

    update: Homeworks

--- HW_BinarySearch.py
+++ HW_BinarySearch.py
@@ -5,22 +5,19 @@ def binary_search_re(start, end, target):
     if start > end:
         return 1
     elif target == num:
-        print(num)
         return num
     elif target < num:
-        binary_search_re(start, lst[mid - 1], target)
+        return binary_search_re(start, lst[mid - 1], target)
     else:
-        binary_search_re(lst[mid + 1], end, target)
+        return binary_search_re(lst[mid + 1], end, target)
 
 
 if __name__ == '__main__':
     start = 1
     end = 11
     target = 2
-    result = binary_search_re(1, 11, 7)
-    # print('test:',binary_search_re(start,end,target))
+    result = binary_search_re(start, end, target)
     if result:
-        # pass
         print(result)
     else:
         print("There's no data.")
